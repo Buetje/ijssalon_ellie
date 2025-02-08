52b32ca6d4ebddd90904c597aa4dc3900cd2b9ed
Justin Debue <justin.debue@hotmail.com>
2025-02-08 15:26:28 +0100

    bestand tijdelijk.py gecodeerd

--- tijdelijk.py
+++ tijdelijk.py
@@ -8,4 +8,9 @@ reclame_tekst = f"Vandaag in de aanbieding: vanille-ijs, 1 liter – slechts €
 aanbieding_str = str(aanbieding)
 reclame_tekst2 = reclame_tekst[:reclame_tekst.index('2')+2]
 reclame_tekst3 = reclame_tekst2.upper()
-print (reclame_tekst3)
+reclame_tekst4 = reclame_tekst3.split()
+for el in reclame_tekst4:
+    if len(el) >= 5:
+        print(el.upper())
+    else:
+        print(el.lower())
